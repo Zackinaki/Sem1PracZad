631f0c456e4f1791c03d6d1cc5c06eeb04587b77
IrinchinovZh <exp.bur@gmail.com>
2022-11-22 12:57:33 +0800

    Add zad6

---
+++ Zad6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Vvedite chislo: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a%2==0)
+Console.Write("Chislo chetnoe");
+else
+Console.Write("Chislo nechetnoe");
