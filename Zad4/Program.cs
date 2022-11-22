91b172aa3393dc195a81e28c0afa70fafecb9aa9
IrinchinovZh <exp.bur@gmail.com>
2022-11-22 12:41:34 +0800

    Add zad4

---
+++ Zad4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Vvedite chislo pervoe: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Vvedite chislo vtoroe: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Vvedite chislo tret'e: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if(max>b)
+max=a;
+else
+max=b;
+if(max>c)
+{
+Console.Write("max chislo ");
+Console.Write(max);
+}
+else
+{
+max=c;
+Console.Write("max chislo ");
+Console.Write(max);
+}
