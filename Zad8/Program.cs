f1ea4e3abe17bfd99055fd418451f926d3a11525
IrinchinovZh <exp.bur@gmail.com>
2022-11-22 14:41:02 +0800

    Add zad8

---
+++ Zad8/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+Console.Write("Vvedite chislo: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i =1;
+while (n<1)
+{
+    Console.Write("Vvdedite chislo ot 1: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write($"Chetnii chisla ot 1 do {n}: ");
+if(n==1)
+    {
+        Console.Write("netu");
+    }
+while(i<=n)
+{
+    
+    if(i%2==0)
+    {
+        Console.Write(i +" ");
+        i++;
+    }
+    else
+    i++;
+}
